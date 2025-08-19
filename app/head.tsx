5edb7494ccc009957ff7ae689cd28c10c5de6ff1
DivyanshVerma0 <divyansh_verma@icloud.com>
2025-08-19 09:29:52 +0530

    Initial commit

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Exonium</title>
+      <meta
+        name="description"
+        content="Exonium, A Project manager"
+      />
+      <meta name="robots" content="noindex" />
+    </>
+  );
+}
